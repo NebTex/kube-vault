dd7fa884d91c4b5b39ec77a051e1d54b88902945
Cristian Lozano <criscalovis@gmail.com>
2016-12-23 02:18:41 -0500

    Initial commit

--- remove.sh
+++ remove.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-kubectl delete -f vault-namespace.yml
-kubectl delete -f vault-deployment.yml
 kubectl delete -f vault-service.yml
+kubectl delete -f vault-deployment.yml
+kubectl delete -f vault-namespace.yml
